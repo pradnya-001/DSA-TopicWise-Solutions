6aa3e8f24af7c88834f5e070e72197921e62e8c3
pradnya-001 <pradnyapangavhane23@gmail.com>
2025-10-19 18:12:27 +0530

    Added new DSA topic or updated code

--- Arrays/loops_on_array.cpp
+++ Arrays/loops_on_array.cpp
@@ -2,10 +2,21 @@
  using namespace std;
 
 int main() {
-     int marks[] = {10, 20, 30, 40, 50};
-     int size = sizeof(marks)/ sizeof(int) ;
-     for(int i = 0 ; i<size;i++){
+    //  int marks[] = {10, 20, 30, 40, 50};
+    //  int size = sizeof(marks)/ sizeof(int) ;
+    //  for(int i = 0 ; i<size;i++){
+    //     cout << marks[i] << endl;
+    //  }
+
+    int marks[5];
+    int size = 5;
+    for(int i = 0;i <size;i++){
+        cin >> marks[i];
+    }
+    
+
+    for(int i = 0;i<size;i++){
         cout << marks[i] << endl;
-     }
+    }
     return 0;
 }
