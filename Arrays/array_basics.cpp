07f8741aa6c22cf2da4fba92dae8ad45c312f8a8
pradnya-001 <pradnyapangavhane23@gmail.com>
2025-10-19 17:47:23 +0530

    Added new DSA topic or updated code

---
+++ Arrays/array_basics.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+ using namespace std;
+
+int main() {
+   int marks[] = {10,20,30,40,50};
+
+   cout << "Marks of student 1: " << marks[0] << endl;
+   cout << "Marks of student 2: " << marks[1] << endl;  
+    cout << "Marks of student 3: " << marks[2] << endl;
+    cout << "Marks of student 4: " << marks[3] << endl;
+    cout << "Marks of student 5: " << marks[4] << endl;
+    
+    return 0;
+}
