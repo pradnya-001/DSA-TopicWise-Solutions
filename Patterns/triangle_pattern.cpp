9a89f84bd14d307b85ef6bc4dc13f58632c472b1
pradnya-001 <pradnyapangavhane23@gmail.com>
2025-10-19 09:29:09 +0530

    Added new DSA topic or updated code

--- Patterns/triangle_pattern.cpp
+++ Patterns/triangle_pattern.cpp
@@ -4,8 +4,10 @@
 int main() {
     int n = 5;
     for (int i = 1; i <= n; ++i) {
-        for (int j = 1; j <= i; ++j) {
-            cout << "* ";
+        char ch = 'A';
+        for (int j = 1; j <= n; ++j) {
+            cout << ch << " ";
+            ch++;
         }
         cout << endl;
     }
