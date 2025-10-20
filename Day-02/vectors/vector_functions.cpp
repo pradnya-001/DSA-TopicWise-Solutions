1310ad07b59c382f9c760e5096c9010303c27724
pradnya-001 <pradnyapangavhane23@gmail.com>
2025-10-20 18:06:02 +0530

    Added new DSA topic or updated code

---
+++ Day-02/vectors/vector_functions.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <vector>
+ using namespace std;
+
+int main() {
+    vector <char> alpha = {'a','b','c','d'};
+    int size = alpha.size();
+    cout << "size of vector is:" << size << endl;
+    return 0;
+}
