798cf326d6acd561a5a9e9445c61b8e13ef41ee2
pradnya-001 <pradnyapangavhane23@gmail.com>
2025-10-20 10:40:50 +0530

    Added new DSA topic or updated code

---
+++ Day-01/Arrays/reverse_an_array.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+ using namespace std;
+
+ int reverseArray(int arr[],int n){
+    int start = 0,end = n-1;
+    while(start < end){
+        swap(arr[start],arr[end]);
+        start++;
+        end--;
+    }
+ }
+
+int main() {
+    int arr[] = {10, 20, 30, 40, 50};
+    int n = 5;
+
+    reverseArray(arr ,n);
+
+    for(int i = 0; i< n ;i++){
+        cout << arr[i] <<" ";
+    }
+    cout << endl;
+
+    return 0;
+}
