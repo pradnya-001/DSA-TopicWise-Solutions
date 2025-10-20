6c8de9e4386cbfccee2c67bab4ae7af6ca7bf150
pradnya-001 <pradnyapangavhane23@gmail.com>
2025-10-20 17:59:35 +0530

    Added new DSA topic or updated code

--- Day-02/vectors/vector_basic.cpp
+++ Day-02/vectors/vector_basic.cpp
@@ -4,8 +4,8 @@
 
 int main() {
     vector<int> vec = {10, 20, 30, 40, 50};
-    for(int i = 0; i < vec.size(); i++) {
-        cout << vec[i] << endl;
+    for(int i : vec) {
+        cout << i << endl;
     }
     return 0;
 }
