b7528695b074ddd8ca1ded4b9e5ac8d1e4c16a7f
pradnya-001 <pradnyapangavhane23@gmail.com>
2025-10-21 10:11:05 +0530

    Added new DSA topic or updated code

--- Day-03/pair_sum_opti.cpp
+++ Day-03/pair_sum_opti.cpp
@@ -1,11 +1,37 @@
 #include <iostream>
-#include<vector>
- using namespace std;
+#include <vector>
+using namespace std;
 
- // pair sum in sorted array
- //two pointer approach
+vector<int> Pairsum(vector<int> nums, int target) {
+    int i = 0;
+    int n = nums.size();
+    int j = n - 1;
+
+    while (i < j) {
+        int sum = nums[i] + nums[j];
+        if (sum == target) {
+            return {i, j};
+        }
+        else if (sum > target) {
+            j--;
+        }
+        else {
+            i++;
+        }
+    }
+    return {-1, -1}; // No pair found
+}
 
 int main() {
-    std::cout << "" << std::endl;
+    vector<int> nums = {2, 3, 4, 5, 7, 8, 9};
+    int target = 7;
+
+    vector<int> ans = Pairsum(nums, target);
+
+    if (ans[0] != -1)
+        cout << "Pair found at indices: " << ans[0] << " and " << ans[1] << endl;
+    else
+        cout << "No pair found for target " << target << endl;
+
     return 0;
 }
