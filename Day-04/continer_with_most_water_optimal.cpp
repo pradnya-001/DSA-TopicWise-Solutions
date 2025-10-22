816e7763ff068571356f8488354e714b09e88039
pradnya-001 <pradnyapangavhane23@gmail.com>
2025-10-22 17:28:23 +0530

    Added new DSA topic or updated code

---
+++ Day-04/continer_with_most_water_optimal.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int maxArea(vector<int>& height) {
+       int maxWater = 0;
+       int lp = 0, rp = height.size() - 1;
+       while(lp< rp){
+              int wt = rp - lp;
+              int ht = min(height[lp], height[rp]);
+              int currWater = wt * ht;
+    
+              maxWater = max(maxWater, currWater);
+    
+            //   if(height[lp] < height[rp]){
+            //     lp++;
+            //   }
+            //   else{
+            //     rp--;
+            //   }
+       }
+    }
+};
+
+int main() {
+    Solution sol;
+    vector<int> height = {1,8,6,2,5,4,8,3,7};
+    cout << "Maximum area: " << sol.maxArea(height) << endl;
+    return 0;
+}
+//time complexity: O(n)
