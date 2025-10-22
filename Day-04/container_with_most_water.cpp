08e730d780b8c8689c6fcc2792ddc733bf17bc7d
pradnya-001 <pradnyapangavhane23@gmail.com>
2025-10-22 16:36:53 +0530

    Added new DSA topic or updated code

---
+++ Day-04/container_with_most_water.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int maxArea(vector<int>& height) {
+        int maxWater = 0;
+        for (int i = 0; i < height.size(); i++) {
+            for (int j = i + 1; j < height.size(); j++) {
+                int wt = j - i; // width between the two lines
+                int ht = min(height[i], height[j]); // height of the smaller line
+                int currWater = wt * ht; // area = width * height
+
+                maxWater = max(maxWater, currWater);
+            }
+        }
+        return maxWater;
+    }
+};
+
+int main() {
+    Solution sol;
+    vector<int> height = {1,8,6,2,5,4,8,3,7};
+    cout << "Maximum area: " << sol.maxArea(height) << endl;
+    return 0;
+}
+//time complexity: O(n^2)
