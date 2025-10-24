f8c53811ad0e591e6f9224a3148d21af60994327
pradnya-001 <pradnyapangavhane23@gmail.com>
2025-10-24 19:26:38 +0530

    Added new DSA topic or updated code

--- Day-05/product_of_array_optimal.cpp
+++ Day-05/product_of_array_optimal.cpp
@@ -1,7 +1,41 @@
-#include <iostream>
- using namespace std;
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    vector<int> productExceptSelf(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> ans(n, 1);
+        vector<int> preffix(n, 1);
+        vector<int> suffix(n, 1);
+
+        // Build prefix array
+        for(int i = 1; i < n; i++){
+            preffix[i] = preffix[i - 1] * nums[i - 1];
+        }
+
+        // Build suffix array
+        for(int i = n - 2; i >= 0; i--){
+            suffix[i] = suffix[i + 1] * nums[i + 1];
+        }
+
+        // Final answer using prefix and suffix
+        for(int i = 0; i < n; i++){
+            ans[i] = preffix[i] * suffix[i];
+        }
+
+        return ans;
+    }
+};
 
 int main() {
-    std::cout << "" << std::endl;
-    return 0;
+    // Example to test the function
+    vector<int> nums = {1, 2, 3, 4};
+    Solution sol;
+    vector<int> result = sol.productExceptSelf(nums);
+
+    cout << "Product Except Self: ";
+    for(int val : result){
+        cout << val << " ";
+    }
 }
