641bbea4f2082f9a001477480131bab5b17ad666
pradnya-001 <pradnyapangavhane23@gmail.com>
2025-10-26 20:18:06 +0530

    Added new DSA topic or updated code

--- Day-06/pointers/pass_by_reference_using_pointers.cpp
+++ Day-06/pointers/pass_by_reference_using_pointers.cpp
@@ -1,14 +1,14 @@
 #include <iostream>
  using namespace std;
 
- void change(int a){
-    a = 20;
+ void change(int* ptr){
+    *ptr = 20;
  }
 
 int main() {
     int a =10;
     cout << "before function call:" << a << endl;
-    change(a);
+    change(&a);
     cout <<"after function call:" << a << endl;
     return 0;
 }
