8277b5b399e48ff070bd5c1a62c216e9f10a8686
pradnya-001 <pradnyapangavhane23@gmail.com>
2025-10-26 20:00:52 +0530

    Added new DSA topic or updated code

--- Day-06/pointers/pointesrs_basic.cpp
+++ Day-06/pointers/pointesrs_basic.cpp
@@ -1,38 +1,36 @@
 #include <iostream>
- using namespace std;
+using namespace std;
 
 int main() {
-//    int a=10;
-//    int* ptr = &a; // pointer variable ptr holds the address of variable a
-//    cout << &a << endl;  // prints the address of variable a
-//    cout << ptr << endl; // prints the address stored in pointer variable ptr
-//    cout << &ptr << endl; 
+   int a = 10;
+   int* ptr = &a; // pointer variable ptr holds the address of variable a
 
+   cout << &a << endl;   // prints the address of variable a
+   cout << ptr << endl;  // prints the address stored in pointer variable ptr
+   cout << &ptr << endl; // prints the address of pointer ptr
 
-//    float price = 2.3456;
-//    float* p = &price;
-//    cout << "Address of price: " << &price << endl;
-//    cout << "Address stored in pointer p: " << p << endl;
-//    cout << "Address of pointer p: " << &p << endl;
+   float price = 2.3456;
+   float* p = &price;
+   cout << "Address of price: " << &price << endl;
+   cout << "Address stored in pointer p: " << p << endl;
+   cout << "Address of pointer p: " << &p << endl;
 
+   // Pointer to pointer concept
+   float** p2 = &p;
+   cout << "Value stored in pointer p2 (address of p): " << p2 << endl;
 
-//    //pointer to pointer concept
-//     float** p2 = &p;
-//     cout << "Address at pointer p2: " << p2 << endl;
+   // Dereferencing operator (*)
+   int b = 100;
+   int* p3 = &b;
+   int** p4 = &p3;
 
+   cout << *p3 << endl;     // prints 100
+   cout << *(&b) << endl;   // prints 100
+   cout << **p4 << endl;    // prints 100
 
-//Dereferencing operator (*)
-    // int a = 100;
-    // int* p = &a;
-    // int** p2 = &p;
-    // cout << *(p) << endl;
-    // cout << *(&a) << endl; // dereferencing the address of a
-    // cout << **(p2) << endl; // dereferencing pointer to pointer
+   // NULL Pointer
+   int* nullPtr = NULL;
+   cout << nullPtr << endl; // prints 0 or NULL address
 
-
-    // NULL Pointer
-    int **ptr = NULL;
-    cout << ptr << endl;
-   
-    return 0;
+   return 0;
 }
