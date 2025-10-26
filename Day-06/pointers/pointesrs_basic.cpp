db0304670996e541a773e92881937900cf4f6f86
pradnya-001 <pradnyapangavhane23@gmail.com>
2025-10-26 19:28:18 +0530

    Added new DSA topic or updated code

--- Day-06/pointers/pointesrs_basic.cpp
+++ Day-06/pointers/pointesrs_basic.cpp
@@ -2,23 +2,30 @@
  using namespace std;
 
 int main() {
-   int a=10;
-   int* ptr = &a; // pointer variable ptr holds the address of variable a
-   cout << &a << endl;  // prints the address of variable a
-   cout << ptr << endl; // prints the address stored in pointer variable ptr
-   cout << &ptr << endl; 
+//    int a=10;
+//    int* ptr = &a; // pointer variable ptr holds the address of variable a
+//    cout << &a << endl;  // prints the address of variable a
+//    cout << ptr << endl; // prints the address stored in pointer variable ptr
+//    cout << &ptr << endl; 
 
 
-   float price = 2.3456;
-   float* p = &price;
-   cout << "Address of price: " << &price << endl;
-   cout << "Address stored in pointer p: " << p << endl;
-   cout << "Address of pointer p: " << &p << endl;
+//    float price = 2.3456;
+//    float* p = &price;
+//    cout << "Address of price: " << &price << endl;
+//    cout << "Address stored in pointer p: " << p << endl;
+//    cout << "Address of pointer p: " << &p << endl;
 
 
-   //pointer to pointer concept
-    float** p2 = &p;
-    cout << "Address at pointer p2: " << p2 << endl;
+//    //pointer to pointer concept
+//     float** p2 = &p;
+//     cout << "Address at pointer p2: " << p2 << endl;
+
+
+//Dereferencing operator (*)
+    int a = 10;
+    int* p = &a;
+    cout << *(p) << endl;
+
    
     return 0;
 }
