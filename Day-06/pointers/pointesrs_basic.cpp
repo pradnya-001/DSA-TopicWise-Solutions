ce521605eca71e4ac2c6e38982786a721e82dab1
pradnya-001 <pradnyapangavhane23@gmail.com>
2025-10-26 19:12:56 +0530

    Added new DSA topic or updated code

--- Day-06/pointers/pointesrs_basic.cpp
+++ Day-06/pointers/pointesrs_basic.cpp
@@ -3,6 +3,8 @@
 
 int main() {
    int a=10;
+   int* ptr = &a; // pointer variable ptr holds the address of variable a
    cout << &a << endl;  // prints the address of variable a
+   cout << ptr << endl; // prints the address stored in pointer variable ptr
     return 0;
 }
