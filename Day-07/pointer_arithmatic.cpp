ea93995d8ddcc74d5d2458f892f19afc6f7cd2d6
pradnya-001 <pradnyapangavhane23@gmail.com>
2025-10-27 18:01:34 +0530

    Added new DSA topic or updated code

--- Day-07/pointer_arithmatic.cpp
+++ Day-07/pointer_arithmatic.cpp
@@ -4,14 +4,22 @@
 int main() {
     //Increment and Decrement Operations
 
-    int a = 10;
-    int* ptr = &a;
-    cout << ptr << endl;
-    ptr--;
-    cout << ptr << endl;
+    // int a = 10;
+    // int* ptr = &a;
+    // cout << ptr << endl;
+    // ptr--;
+    // cout << ptr << endl;
 
-    //Add and Sub
-    ptr = ptr + 2;
-    cout << ptr << endl;
+    // //Add and Sub
+    // ptr = ptr + 2;
+    // cout << ptr << endl;
+
+// in cpp we are not allowed to add the two pointers(ptr1 + ptr2)
+// but we are allowed to substract the one pointer value from another (ptr2 - ptr1) if the type of the two pointer is same
+
+    int* ptr2; //100
+    int* ptr1 = ptr2 + 2; //108
+    cout << ptr1 - ptr2 << endl;
+
     return 0;
 }
