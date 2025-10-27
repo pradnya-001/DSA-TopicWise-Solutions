7d051e84e1fd1288f2b4f5ac5873e6f96d6526cf
pradnya-001 <pradnyapangavhane23@gmail.com>
2025-10-27 17:40:53 +0530

    Added new DSA topic or updated code

--- Day-06/pointers/array_pointers.cpp
+++ Day-06/pointers/array_pointers.cpp
@@ -5,5 +5,9 @@ int main() {
      int arr[] = {10, 20, 30, 40, 50};
      cout << "Array elements using pointer arithmetic:" << endl;
      cout << *(arr) << endl; // pointer to first element
+        cout << *(arr + 1) << endl; // pointer to second element
+        cout << *(arr + 2) << endl; // pointer to third element
+        cout << *(arr + 3) << endl; // pointer to fourth element
+        cout << *(arr + 4) << endl; // pointer to fifth element
     return 0;
 }
