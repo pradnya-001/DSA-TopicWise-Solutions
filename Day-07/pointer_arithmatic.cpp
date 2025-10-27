52782b9fee7963670541cee43d6830fe0fcf4fb5
pradnya-001 <pradnyapangavhane23@gmail.com>
2025-10-27 17:47:10 +0530

    Added new DSA topic or updated code

--- Day-07/pointer_arithmatic.cpp
+++ Day-07/pointer_arithmatic.cpp
@@ -2,6 +2,12 @@
  using namespace std;
 
 int main() {
-    std::cout << "" << std::endl;
+    //Increment and Decrement Operations
+
+    int a = 10;
+    int* ptr = &a;
+    cout << ptr << endl;
+    ptr++;
+    cout << ptr << endl;
     return 0;
 }
