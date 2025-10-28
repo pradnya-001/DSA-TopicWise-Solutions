0056add523b83af415a949f9f03450ffffd80609
pradnya-001 <pradnyapangavhane23@gmail.com>
2025-10-28 18:02:57 +0530

    Added new DSA topic or updated code

--- Day-08/540_Single_element_in_sorted_array.cpp
+++ Day-08/540_Single_element_in_sorted_array.cpp
@@ -1,7 +1,32 @@
 #include <iostream>
- using namespace std;
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    int singleNonDuplicate(vector<int>& nums) {
+        int n = nums.size();
+
+        // Edge cases
+        if(n == 1) return nums[0];
+        if(nums[0] != nums[1]) return nums[0];
+        if(nums[n-1] != nums[n-2]) return nums[n-1];
+
+        // Linear search
+        for(int i = 1; i < n-1; i++) {
+            if(nums[i] != nums[i-1] && nums[i] != nums[i+1]) {
+                return nums[i];
+            }
+        }
+        
+        return -1; // Just in case (all you have valid input)
+    }
+};
 
 int main() {
-    std::cout << "" << std::endl;
+    vector<int> nums = {1,1,2,3,3,4,4,8,8}; // Sample Input
+    Solution obj;
+
+    cout << "Single Element: " << obj.singleNonDuplicate(nums) << endl;
     return 0;
 }
