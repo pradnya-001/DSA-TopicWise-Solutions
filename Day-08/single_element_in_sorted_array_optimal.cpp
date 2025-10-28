5db5361e64cb56a93bf520f486bf13eee1f8062f
pradnya-001 <pradnyapangavhane23@gmail.com>
2025-10-28 18:02:01 +0530

    Added new DSA topic or updated code

--- Day-08/single_element_in_sorted_array_optimal.cpp
+++ Day-08/single_element_in_sorted_array_optimal.cpp
@@ -1,7 +1,30 @@
 #include <iostream>
- using namespace std;
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    int singleNonDuplicate(vector<int>& nums) {
+        int left = 0, right = nums.size() - 1;
+        while(left < right) {
+            int mid = (left + right) / 2;
+
+            // Check pairing pattern
+            if((mid % 2 == 0 && nums[mid] == nums[mid + 1]) ||
+               (mid % 2 == 1 && nums[mid] == nums[mid - 1])) {
+                left = mid + 1;
+            } else {
+                right = mid;
+            }
+        }
+        return nums[left];
+    }
+};
 
 int main() {
-    std::cout << "" << std::endl;
+    vector<int> nums = {1,1,2,3,3,4,4,8,8}; // Sample Input
+    Solution obj;
+    
+    cout << "Single Element: " << obj.singleNonDuplicate(nums) << endl;
     return 0;
 }
