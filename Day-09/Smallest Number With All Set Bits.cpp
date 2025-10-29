a64db31de7c057a89159e950ee26e784b839c45e
pradnya-001 <pradnyapangavhane23@gmail.com>
2025-10-29 21:19:54 +0530

    Added new DSA topic or updated code

---
+++ Day-09/Smallest Number With All Set Bits.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+class Solution {
+public:
+    int smallestNumber(int n) {
+        int x = n;
+        while ((x & (x + 1)) != 0) {
+            x++;
+        }
+        return x;
+    }
+};
+
+int main() {
+    Solution sol;
+    int n;
+    cout << "Enter a number: ";
+    cin >> n;
+
+    int result = sol.smallestNumber(n);
+    cout << "Smallest number >= " << n << " such that x & (x+1) == 0 : " << result << endl;
+
+    return 0;
+}
