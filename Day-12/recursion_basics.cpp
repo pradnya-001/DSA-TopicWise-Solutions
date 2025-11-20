248aa215fb104cb6e612bde45091db9af0504d2d
pradnya-001 <pradnyapangavhane23@gmail.com>
2025-11-20 10:32:05 +0530

    Added new DSA topic or updated code

--- Day-12/recursion_basics.cpp
+++ Day-12/recursion_basics.cpp
@@ -2,10 +2,14 @@
  using namespace std;
  //what is recursion?
  //Recursion is a programming technique where a function calls itself to solve a smaller instance of the same problem.
+//in simple words function calling itself until a certain condition is met.
 
+//Types of recursion
+//1. Direct Recursion: A function calls itself directly.
+//2. Indirect Recursion: A function calls another function, which in turn calls the first function.
 
  //This is the example of the infinite recursion or non terminating recursion
-
+// infinite recursion leads to the stack overflow ie, segmentation fault.
  void printMessages(){
     std::cout << "Hello, World!" << std::endl;
     std::cout << "Welcome to C++ programming." << std::endl;
