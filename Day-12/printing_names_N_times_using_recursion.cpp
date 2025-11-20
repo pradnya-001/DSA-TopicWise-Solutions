4bafe594e7ed2b28709fda7a6fcfd2555633e134
pradnya-001 <pradnyapangavhane23@gmail.com>
2025-11-20 11:16:37 +0530

    Added new DSA topic or updated code

--- Day-12/printing_names_N_times_using_recursion.cpp
+++ Day-12/printing_names_N_times_using_recursion.cpp
@@ -1,7 +1,22 @@
 #include <iostream>
  using namespace std;
+ void printNameNTimes(int n) {
+    // Base Condition
+    if (n <= 0) {
+        return;
+    }
+
+    // Print the name
+    cout << "Pradnya" << endl;
+
+    // Recursive Call
+    printNameNTimes(n - 1);
+ }
+
 
 int main() {
-    std::cout << "" << std::endl;
+    int n;
+    cin >> n;
+    printNameNTimes(n);
     return 0;
 }
