773c543cc455e3b30123d86ea1c2906562c9fc25
pradnya-001 <pradnyapangavhane23@gmail.com>
2025-11-20 10:36:08 +0530

    Added new DSA topic or updated code

---
+++ Day-12/rec.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+ using namespace std;
+
+ //the condition where the execution of the function stops is known as the base condition 
+
+int main() {
+    std::cout << "" << std::endl;
+    return 0;
+}
