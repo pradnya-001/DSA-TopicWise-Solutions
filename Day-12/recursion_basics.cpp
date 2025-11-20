be0a9fa4af9abf8ecd131f625d7777f85e355241
pradnya-001 <pradnyapangavhane23@gmail.com>
2025-11-20 10:26:04 +0530

    Added new DSA topic or updated code

--- Day-12/recursion_basics.cpp
+++ Day-12/recursion_basics.cpp
@@ -1,5 +1,10 @@
 #include <iostream>
  using namespace std;
+ //what is recursion?
+ //Recursion is a programming technique where a function calls itself to solve a smaller instance of the same problem.
+
+
+ //This is the example of the infinite recursion or non terminating recursion
 
  void printMessages(){
     std::cout << "Hello, World!" << std::endl;
