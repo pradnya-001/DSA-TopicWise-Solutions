8078b4f090b13b002bfb3d07648529c05588d6e6
pradnya-001 <pradnyapangavhane23@gmail.com>
2025-11-21 07:07:03 +0530

    Added new DSA topic or updated code

--- Day-12/printing_numbers_in_reverse_order.cpp
+++ Day-12/printing_numbers_in_reverse_order.cpp
@@ -1,19 +1,20 @@
 #include <iostream>
- using namespace std;
+using namespace std;
 
-
-void print(int i , int n){
-    if(i < 1){
+void print(int i, int n){
+    if(i < 1){        // base case: stop when i reaches 0
         return;
     }
-    print(i-1,n);
-   cout <<" "<< i;
+
+    cout << i << " "; // THEN print while backtracking
+    print(i - 1, n);  // FIRST go deep (i=5→4→3→2→1→0)
+
 }
 
-
 int main() {
-    int n ;
+    int n;
     cin >> n;
-    print(n , n );
+
+    print(n, n);
     return 0;
 }
