56e6b843fcd8d123cce8f1ee814a820a7f7b6ace
pradnya-001 <pradnyapangavhane23@gmail.com>
2025-11-22 17:04:55 +0530

    Added new DSA topic or updated code

--- Day-12/sum_of_firdt_n_numbers.cpp
+++ Day-12/sum_of_firdt_n_numbers.cpp
@@ -1,7 +1,18 @@
 #include <iostream>
- using namespace std;
+using namespace std;
+
+void currentSum(int n, int s){   // ← changed 'sum' to 's'
+    if(n < 0){
+        cout << s << endl;
+        return;
+    }
+    currentSum(n - 1, s + n);
+}
 
 int main() {
-    std::cout << "" << std::endl;
+    int n;
+    cout << "Enter a positive integer: ";
+    cin >> n;
+    currentSum(n, 0);
     return 0;
 }
